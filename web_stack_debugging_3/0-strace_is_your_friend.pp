d1a798f3716721035a332a11a5405bc152a28980
Amandine0610 <a.irakoze@alustudent.com>
2024-03-30 03:01:36 -0700

    Fixing issue using Puppet

---
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+#Puppet manifest to automate the modification of a WordPress configuration file
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
---
+++ web_stack_debugging_3/README.md
@@ -0,0 +1,1 @@
+Using strace, finding out why Apache is returning a 500 error. Once the issue is found, fixing it and then automating it using Puppet
